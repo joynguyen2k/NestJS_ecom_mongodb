74398c85efe6a90be8ad58d447e7828158b82e3e
joynguyen2k <lamntt@vmodev.com>
2022-04-14 13:59:29 +0700

    upload file image

--- my-app/src/app.module.ts
+++ my-app/src/app.module.ts
@@ -8,10 +8,21 @@ import { UsersModule } from './users/users.module';
 import { OrderItemsModule } from './order-items/order-items.module';
 import { OrderModule } from './order/order.module';
 import { CategoryModule } from './category/category.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { UploadModule } from './upload/upload.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/joyshop_ecom'), ProductModule, AuthModule, UsersModule, OrderModule, CategoryModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/joyshop_ecom'), 
+    MulterModule.register({
+      dest: './filesload',
+    }),
+    ProductModule, 
+    AuthModule, 
+    UsersModule, 
+    OrderModule, 
+    CategoryModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
